c7c1a107ceedd51023046c40ebcd2b2032104f13
Ibrahem Jasim Alsalih <alazawy.m91@gmail.com>
2025-12-04 14:15:19 +0100

    Implement welcome screen logic and add GameActivity Adds the `games-activity` library to the project dependencies. Uses ViewBinding to access UI

--- app/src/main/java/com/example/blcakjack/MainActivity.kt
+++ app/src/main/java/com/example/blcakjack/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.blcakjack
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.blcakjack.databinding.ActivityMainBinding
+import com.google.androidgamesdk.GameActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStartGame.setOnClickListener {
+            val name = binding.etName.text.toString().trim().ifEmpty { "Player" }
+            val intent = Intent(this, GameActivity::class.java)
+            intent.putExtra("player_name", name)
+            startActivity(intent)
+
         }
+
+
     }
 }
